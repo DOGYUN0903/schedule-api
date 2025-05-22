2a3c9ebcc24c0f45ea07ce7cc0728a2e95758ed0
SexyDoGyun <kimdk0903@naver.com>
2025-05-22 11:41:40 +0900

    feat: 로그인 인증용 LoginFilter 구현 및 필터 등록 설정 추가

---
+++ src/main/java/com/example/schedule/global/filter/LoginFilter.java
@@ -0,0 +1,49 @@
+package com.example.schedule.global.filter;
+
+import com.example.schedule.global.session.SessionConst;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.PatternMatchUtils;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginFilter implements Filter {
+
+    private static final String[] WHITE_LIST = {
+            "/api/v2/members",
+            "/api/v2/members/login"
+    };
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain
+    ) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String requestURI = httpRequest.getRequestURI();
+        log.info("로그인 필터 실행 URI = {}", requestURI);
+
+        if (!isWhiteList(requestURI)) {
+            HttpSession session = httpRequest.getSession(false);
+
+            if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private boolean isWhiteList(String requestURI) {
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+    }
+}
