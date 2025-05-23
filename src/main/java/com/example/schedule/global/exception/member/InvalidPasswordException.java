1905c19ec441b3da24eaaa1b60a11432104e05bb
SexyDoGyun <kimdk0903@naver.com>
2025-05-23 09:50:40 +0900

    refactor: InvalidPasswordException이 메시지를 받아오도록 수정

--- src/main/java/com/example/schedule/global/exception/member/InvalidPasswordException.java
+++ src/main/java/com/example/schedule/global/exception/member/InvalidPasswordException.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 
 public class InvalidPasswordException extends CustomException {
 
-    public InvalidPasswordException() {
-        super("비밀번호가 일치하지 않습니다.");
+    public InvalidPasswordException(String message) {
+        super(message);
     }
 
     @Override
