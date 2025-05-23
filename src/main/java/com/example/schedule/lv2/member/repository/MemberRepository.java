5d7290a691bdb0394d5dbc2428624959d0e98643
SexyDoGyun <kimdk0903@naver.com>
2025-05-23 14:29:11 +0900

    refactor: MemberRepository에서 예외를 던지던 것을 MemberService에서 예외를 던지도록 수정

--- src/main/java/com/example/schedule/lv2/member/repository/MemberRepository.java
+++ src/main/java/com/example/schedule/lv2/member/repository/MemberRepository.java
@@ -10,24 +10,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    default Member findByIdOrElseThrow(Long id) {
-        return findById(id)
-                .orElseThrow(() ->
-                        new MemberNotFoundException("존재하지 않는 회원입니다.")
-                );
-    }
-
     boolean existsByEmail(String email);
 
     Member findByEmail(String email);
-
-    default Member findByEmailOrElseThrow(String email) {
-        Member member = findByEmail(email);
-
-        if (member == null) {
-            throw new EmailNotFoundException("해당 이메일의 회원이 존재하지 않습니다.");
-        }
-        return member;
-    }
 
 }
--- src/main/java/com/example/schedule/lv2/member/service/MemberService.java
+++ src/main/java/com/example/schedule/lv2/member/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.example.schedule.lv2.member.service;
 
+import com.example.schedule.global.auth.PasswordEncoder;
 import com.example.schedule.global.exception.member.EmailAlreadyExistsException;
 import com.example.schedule.global.exception.common.InvalidPasswordException;
+import com.example.schedule.global.exception.member.EmailNotFoundException;
+import com.example.schedule.global.exception.member.MemberNotFoundException;
 import com.example.schedule.lv2.member.dto.login.LoginRequestDto;
 import com.example.schedule.lv2.member.dto.signup.MemberSignupRequestDto;
 import com.example.schedule.lv2.member.dto.signup.MemberSignupResponseDto;
@@ -19,8 +22,11 @@ import java.util.List;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public MemberSignupResponseDto saveMember(MemberSignupRequestDto requestDto) {
+
+        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
 
         if (memberRepository.existsByEmail(requestDto.getEmail())) {
             throw new EmailAlreadyExistsException("이미 사용 중인 이메일입니다.");
@@ -30,7 +36,7 @@ public class MemberService {
                 new Member(
                         requestDto.getUsername(),
                         requestDto.getEmail(),
-                        requestDto.getPassword()
+                        encodedPassword
                 );
 
         Member savedMember = memberRepository.save(member);
@@ -45,17 +51,18 @@ public class MemberService {
     }
 
     public MemberSignupResponseDto findById(Long id) {
-
-        Member findMember = memberRepository.findByIdOrElseThrow(id);
+        Member findMember = memberRepository.findById(id)
+                .orElseThrow(() -> new MemberNotFoundException("존재하지 않는 회원입니다."));
 
         return new MemberSignupResponseDto(findMember);
     }
 
     @Transactional
     public void updateMember(Long id, MemberUpdateRequestDto updateRequestDto) {
-        Member findMember = memberRepository.findByIdOrElseThrow(id);
+        Member findMember = memberRepository.findById(id)
+                .orElseThrow(() -> new MemberNotFoundException("존재하지 않는 회원입니다."));
 
-        if (!findMember.getPassword().equals(updateRequestDto.getOldPassword())) {
+        if (!passwordEncoder.matches(updateRequestDto.getPassword(), findMember.getPassword())) {
             throw new InvalidPasswordException("비밀번호가 일치하지 않습니다.");
         }
 
@@ -63,9 +70,10 @@ public class MemberService {
     }
 
     public void deleteMember(Long id, String password) {
-        Member findMember = memberRepository.findByIdOrElseThrow(id);
+        Member findMember = memberRepository.findById(id)
+                .orElseThrow(() -> new MemberNotFoundException("존재하지 않는 회원입니다."));
 
-        if (!findMember.getPassword().equals(password)) {
+        if (!passwordEncoder.matches(password, findMember.getPassword())) {
             throw new InvalidPasswordException("비밀번호가 일치하지 않습니다.");
         }
 
@@ -74,9 +82,13 @@ public class MemberService {
 
 
     public Member login(LoginRequestDto requestDto) {
-        Member findMember = memberRepository.findByEmailOrElseThrow(requestDto.getEmail());
 
-        if (!findMember.getPassword().equals(requestDto.getPassword())) {
+        Member findMember = memberRepository.findByEmail(requestDto.getEmail());
+        if (findMember == null) {
+            throw new EmailNotFoundException("해당 이메일의 회원이 존재하지 않습니다.");
+        }
+
+        if (!passwordEncoder.matches(requestDto.getPassword(), findMember.getPassword())) {
             throw new InvalidPasswordException("비밀번호가 일치하지 않습니다.");
         }
 
